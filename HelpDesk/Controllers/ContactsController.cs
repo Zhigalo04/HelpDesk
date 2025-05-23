1af611ab0bcdcbcbc56ef877f044497f8aa667c7
Zhigalo04 <vladzigalo@gmail.com>
2025-05-23 20:57:09 +0300

    Добавьте файлы проекта.

---
+++ HelpDesk/Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿using HelpDesk.Models;
+using HelpDesk.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(contact);
+                _context.SaveChanges();
+
+                return RedirectToAction("Success");
+            }
+
+            return View("Index", contact);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HelpDesk/Models/Contacts.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+            ErrorMessage = "Почта должна содержать символ @ и быть в формате example@domain.com")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [StringLength(1000, ErrorMessage = "Текст должен быть не более 1000 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        public string Message { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
